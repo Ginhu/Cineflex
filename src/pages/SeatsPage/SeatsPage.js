9151af8e94503f882a35202c1d31d5ba5e2e9e9f
Sergio Beppu <beppu.sergio@gmail.com>
2023-03-10 11:27:36 -0300

    Componentização dos styled components

--- src/pages/SeatsPage/SeatsPage.js
+++ src/pages/SeatsPage/SeatsPage.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import styled from "styled-components"
+import {PageContainer, SeatsContainer, FormContainer, CaptionContainer, CaptionItem, FooterContainer} from "./SeatsPageStyled"
 
 export default function SeatsPage(props) {
 
@@ -110,113 +111,34 @@ export default function SeatsPage(props) {
     )
 }
 
-const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-bottom: 120px;
-    padding-top: 70px;
-`
-const SeatsContainer = styled.div`
-    width: 330px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-    margin-top: 20px;
-`
-const FormContainer = styled.div`
-    width: calc(100vw - 40px); 
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin: 20px 0;
-    font-size: 18px;
-    button {
-        align-self: center;
-    }
-    input {
-        width: calc(100vw - 60px);
-    }
-`
-const CaptionContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 300px;
-    justify-content: space-between;
-    margin: 20px;
-`
 const CaptionCircle = styled.div`
-    border: 1px solid ${props=>(props.name === "Selecionado" && "#0E7D71") || (props.name === "Disponível" && "#7B8B99") || (props.name === "Indisponível" && "#F7C52B")};
-    background-color: ${props=>(props.name === "Selecionado" && "#1AAE9E") || (props.name === "Disponível" && "#C3CFD9") || (props.name === "Indisponível" && "#FBE192")};
-    height: 25px;
-    width: 25px;
-    border-radius: 25px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 5px 3px;
+border: 1px solid ${props=>(props.name === "Selecionado" && "#0E7D71") || (props.name === "Disponível" && "#7B8B99") || (props.name === "Indisponível" && "#F7C52B")};
+background-color: ${props=>(props.name === "Selecionado" && "#1AAE9E") || (props.name === "Disponível" && "#C3CFD9") || (props.name === "Indisponível" && "#FBE192")};
+height: 25px;
+width: 25px;
+border-radius: 25px;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 5px 3px;
 `
-const CaptionItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-size: 12px;
-`
+
 const SeatItem = styled.div`
-    border: 1px solid ${props=>(!props.availableSeats.includes(props.name) && "#F7C52B") || (props.selectedSeats.includes(props.name) && "#0E7D71") || (props.availableSeats.includes(props.name) && "#808F9D")};
-    background-color: ${props=>(!props.availableSeats.includes(props.name) && "#FBE192") || (props.selectedSeats.includes(props.name) && "#1AAE9E") || (props.availableSeats.includes(props.name) && "#C3CFD9")};    
-    height: 25px;
-    width: 25px;
-    border-radius: 25px;
-    font-family: 'Roboto';
-    font-size: 11px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 5px 3px;
+border: 1px solid ${props=>(!props.availableSeats.includes(props.name) && "#F7C52B") || (props.selectedSeats.includes(props.name) && "#0E7D71") || (props.availableSeats.includes(props.name) && "#808F9D")};
+background-color: ${props=>(!props.availableSeats.includes(props.name) && "#FBE192") || (props.selectedSeats.includes(props.name) && "#1AAE9E") || (props.availableSeats.includes(props.name) && "#C3CFD9")};    
+height: 25px;
+width: 25px;
+border-radius: 25px;
+font-family: 'Roboto';
+font-size: 11px;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 5px 3px;
 `
-const FooterContainer = styled.div`
-    width: 100%;
-    height: 120px;
-    background-color: #C3CFD9;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    font-size: 20px;
-    position: fixed;
-    bottom: 0;
 
-    div:nth-child(1) {
-        box-shadow: 0px 2px 4px 2px #0000001A;
-        border-radius: 3px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: white;
-        margin: 12px;
-        img {
-            width: 50px;
-            height: 70px;
-            padding: 8px;
-        }
-    }
 
-    div:nth-child(2) {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        p {
-            text-align: left;
-            &:nth-child(2) {
-                margin-top: 10px;
-            }
-        }
-    }
-`
+
+
+
+
--- src/pages/SessionsPage/SessionsPage.js
+++ src/pages/SessionsPage/SessionsPage.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { Link, useParams } from "react-router-dom"
-import styled from "styled-components"
+import { PageContainer, SessionContainer, ButtonsContainer, FooterContainer } from "./SessionsPageStyled"
 
 export default function SessionsPage(props) {
 
@@ -49,76 +49,3 @@ export default function SessionsPage(props) {
         </PageContainer>
     )
 }
-
-const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-bottom: 120px;
-    padding-top: 70px;
-    div {
-        margin-top: 20px;
-    }
-`
-const SessionContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    font-family: 'Roboto';
-    font-size: 20px;
-    color: #293845;
-    padding: 0 20px;
-`
-const ButtonsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin: 20px 0;
-    button {
-        margin-right: 20px;
-    }
-    a {
-        text-decoration: none;
-    }
-`
-const FooterContainer = styled.div`
-    width: 100%;
-    height: 120px;
-    background-color: #C3CFD9;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    font-size: 20px;
-    position: fixed;
-    bottom: 0;
-
-    div:nth-child(1) {
-        box-shadow: 0px 2px 4px 2px #0000001A;
-        border-radius: 3px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: white;
-        margin: 12px;
-        img {
-            width: 50px;
-            height: 70px;
-            padding: 8px;
-        }
-    }
-
-    div:nth-child(2) {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        p {
-            text-align: left;
-            &:nth-child(2) {
-                margin-top: 10px;
-            }
-        }
-    }
-`
